6d8ad3cab767afab7b8d6bd844ae6facfa4a59f0
Dowon Kim <dowoniscool@yahoo.com>
2024-02-10 17:17:02 +0100

    Style : Basic User Interface

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,16 +15,68 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         children: [
           Flexible(
+            //Time
             flex: 1,
-            child: Container(),
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: Text('25:00',
+                  style: TextStyle(
+                    color: Theme.of(context).cardColor,
+                    fontSize: 89,
+                    fontWeight: FontWeight.w600,
+                  )),
+            ),
           ),
           Flexible(
-            flex: 3,
-            child: Container(),
+            //Pomodoro Timer Play Button
+            flex: 2,
+            child: Center(
+              child: IconButton(
+                  icon: const Icon(Icons.play_circle_outlined),
+                  iconSize: 110,
+                  color: Theme.of(context).cardColor,
+                  onPressed: () {}),
+            ),
           ),
           Flexible(
             flex: 1,
-            child: Container(),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Pomodoros',
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.w600,
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .displayLarge!
+                                  .color),
+                        ),
+                        Text(
+                          '0',
+                          style: TextStyle(
+                              fontSize: 55,
+                              fontWeight: FontWeight.w600,
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .displayLarge!
+                                  .color),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
