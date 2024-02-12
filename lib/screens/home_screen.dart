ec8c676f30372405f5c26c4d4002238454f221b7
Dowon Kim <dowoniscool@yahoo.com>
2024-02-12 09:23:59 +0100

    Feat: Play n Pause the timer feature added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +9,45 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Timer? _timer;
+  int _start = 25 * 60; // 25 minutes in seconds
+  bool _isRunning = false;
+
+  void startTimer() {
+    _timer = Timer.periodic(
+      const Duration(seconds: 1),
+      (Timer timer) {
+        if (_isRunning) {
+          setState(() {
+            if (_start < 1) {
+              timer.cancel();
+            } else {
+              _start = _start - 1;
+            }
+          });
+        }
+      },
+    );
+  }
+
+  void resumeAndPauseTimer() {
+    setState(() {
+      _isRunning = !_isRunning;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,12 +59,22 @@ class _HomeScreenState extends State<HomeScreen> {
             flex: 1,
             child: Container(
               alignment: Alignment.bottomCenter,
-              child: Text('25:00',
-                  style: TextStyle(
-                    color: Theme.of(context).cardColor,
-                    fontSize: 89,
-                    fontWeight: FontWeight.w600,
-                  )),
+              child: Text(
+                '${_start ~/ 60}:${(_start % 60).toString().padLeft(2, '0')}',
+                style: TextStyle(
+                  fontSize: 89,
+                  fontWeight: FontWeight.w600,
+                  color: Theme.of(context).cardColor,
+                ),
+              ),
+              // child: Text(
+              //   '25:00',
+              //   style: TextStyle(
+              //     color: Theme.of(context).cardColor,
+              //     fontSize: 89,
+              //     fontWeight: FontWeight.w600,
+              //   ),
+              // ),
             ),
           ),
           Flexible(
@@ -32,10 +82,12 @@ class _HomeScreenState extends State<HomeScreen> {
             flex: 2,
             child: Center(
               child: IconButton(
-                  icon: const Icon(Icons.play_circle_outlined),
+                  icon: _isRunning
+                      ? const Icon(Icons.pause_circle_filled_outlined)
+                      : const Icon(Icons.play_circle_outlined),
                   iconSize: 110,
                   color: Theme.of(context).cardColor,
-                  onPressed: () {}),
+                  onPressed: resumeAndPauseTimer),
             ),
           ),
           Flexible(
@@ -46,7 +98,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Container(
                     decoration: BoxDecoration(
                       color: Theme.of(context).cardColor,
-                      borderRadius: BorderRadius.circular(10),
+                      borderRadius: BorderRadius.circular(50),
                     ),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
