d7c4f427fc3313418d6eb76a05305d40dc785eee
Dowon Kim <dowoniscool@yahoo.com>
2024-02-12 11:03:26 +0100

    Fest : added reset feature

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -36,6 +36,14 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  void resetTimer() {
+    setState(() {
+      _start = 25 * 60;
+      _isRunning = false;
+    });
+    _timer?.cancel();
+  }
+
   @override
   void initState() {
     super.initState();
@@ -52,82 +60,95 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: Column(
-        children: [
-          Flexible(
-            //Time
-            flex: 1,
-            child: Container(
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                '${_start ~/ 60}:${(_start % 60).toString().padLeft(2, '0')}',
-                style: TextStyle(
-                  fontSize: 89,
-                  fontWeight: FontWeight.w600,
-                  color: Theme.of(context).cardColor,
-                ),
-              ),
-              // child: Text(
-              //   '25:00',
-              //   style: TextStyle(
-              //     color: Theme.of(context).cardColor,
-              //     fontSize: 89,
-              //     fontWeight: FontWeight.w600,
-              //   ),
-              // ),
-            ),
-          ),
-          Flexible(
-            //Pomodoro Timer Play Button
-            flex: 2,
-            child: Center(
-              child: IconButton(
-                  icon: _isRunning
-                      ? const Icon(Icons.pause_circle_filled_outlined)
-                      : const Icon(Icons.play_circle_outlined),
-                  iconSize: 110,
-                  color: Theme.of(context).cardColor,
-                  onPressed: resumeAndPauseTimer),
-            ),
-          ),
-          Flexible(
-            flex: 1,
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
+      body: Stack(
+        children: <Widget>[
+          Column(
+            children: [
+              Flexible(
+                //Time
+                flex: 1,
+                child: Container(
+                  alignment: Alignment.bottomCenter,
+                  child: Text(
+                    '${_start ~/ 60}:${(_start % 60).toString().padLeft(2, '0')}',
+                    style: TextStyle(
+                      fontSize: 89,
+                      fontWeight: FontWeight.w600,
                       color: Theme.of(context).cardColor,
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Pomodoros',
-                          style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.w600,
-                              color: Theme.of(context)
-                                  .textTheme
-                                  .displayLarge!
-                                  .color),
-                        ),
-                        Text(
-                          '0',
-                          style: TextStyle(
-                              fontSize: 55,
-                              fontWeight: FontWeight.w600,
-                              color: Theme.of(context)
-                                  .textTheme
-                                  .displayLarge!
-                                  .color),
-                        ),
-                      ],
                     ),
                   ),
+                  // child: Text(
+                  //   '25:00',
+                  //   style: TextStyle(
+                  //     color: Theme.of(context).cardColor,
+                  //     fontSize: 89,
+                  //     fontWeight: FontWeight.w600,
+                  //   ),
+                  // ),
                 ),
-              ],
+              ),
+              Flexible(
+                //Pomodoro Timer Play Button
+                flex: 2,
+                child: Center(
+                  child: IconButton(
+                      icon: _isRunning
+                          ? const Icon(Icons.pause_circle_filled_outlined)
+                          : const Icon(Icons.play_circle_outlined),
+                      iconSize: 110,
+                      color: Theme.of(context).cardColor,
+                      onPressed: resumeAndPauseTimer),
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).cardColor,
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Pomodoros',
+                              style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w600,
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .displayLarge!
+                                      .color),
+                            ),
+                            Text(
+                              '0',
+                              style: TextStyle(
+                                  fontSize: 55,
+                                  fontWeight: FontWeight.w600,
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .displayLarge!
+                                      .color),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            top: 50.0,
+            right: 10.0,
+            child: IconButton(
+              icon: const Icon(Icons.refresh_rounded),
+              color: Theme.of(context).cardColor,
+              onPressed: resetTimer,
             ),
           ),
         ],
