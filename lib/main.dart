dc47d1a0eaa9b30167c15f5f796d385cefba18f8
Dowon Kim <dowoniscool@yahoo.com>
2024-04-18 19:19:13 +0200

    Fix : Fix Home Screen isn't showing as it designed after Splash Screen by uncommented some codes from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,36 +3,30 @@ import 'package:pomo/screens/s_splash.dart';
 import 'screens/s_home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Pomodoro App',
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
-    ),
-    home: const SplashScreen(),
-  ));
+  runApp(const MyApp());
 }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       theme: ThemeData(
-//         textTheme: const TextTheme(
-//           displayLarge: TextStyle(
-//             color: Color(0xFF232B55),
-//           ),
-//         ),
-//         cardColor: const Color(0xFFF4EDDB),
-//         colorScheme: ColorScheme.fromSwatch(
-//           backgroundColor: const Color(0xFFE7627C),
-//         ),
-//       ),
-//       home: const HomeScreen(),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
+        ),
+        cardColor: const Color(0xFFF4EDDB),
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7627C),
+        ),
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
 /*
 Pastel Red : Color(0xFFE7626C)
 Warm white : Color(0xFFF4EDDB)
